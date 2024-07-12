8060790ff37b22cca254ff6af43a0be147923bd6
anton <anton.velitsky@gmail.com>
2024-07-12 17:42:54 +1200

    Add urls with comments

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
 from rest_framework import request
-from menu.views import MenuListView, MenuItemDetailView, TagView
+from menu.views import MenuListView, MenuItemDetailView, TagView, BasketAPIView, OrderAPIView, OrderDetailAPIView
 
 app_name = 'api'
 
@@ -9,4 +9,8 @@ urlpatterns = [
     path('menu/', MenuListView.as_view(), name='menu_api'),
     path('menu/<int:id>/', MenuItemDetailView.as_view(), name='menu-detail'),
     path('tags/', TagView.as_view(), name='tags'),
+    path('basket/', BasketAPIView.as_view(), name='basket'), # to see what is ordered
+    path('basket/<int:id>/', BasketAPIView.as_view(), name='basket_id'), # use id of item and put in basket
+    path('order/', OrderAPIView.as_view(), name='basket_id'), # user can get the list of his orders
+    path('order/<int:id>/', OrderAPIView.as_view(), name='basket_id'), # user can see details of concrete order
 ]
