1f43745a32aa17a719b3d3618a22ced840f9a305
anton <anton.velitsky@gmail.com>
2024-07-13 15:01:52 +1200

    Add menu

--- backend/gico/urls.py
+++ backend/gico/urls.py
@@ -20,6 +20,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
 urlpatterns = [
+    path("", include(("frontend.urls", 'frontend'), namespace='frontend')),
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger_api'),
