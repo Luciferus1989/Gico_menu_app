6eb20e1430a9b54f0b4a8e0a3498ec71d76b5dc3
anton <anton.velitsky@gmail.com>
2024-08-09 13:13:21 +1200

    Add menu

--- backend/gico/settings.py
+++ backend/gico/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'frontend',
     'menu.apps.MenuConfig',
     'myauth.apps.MyauthConfig',
     'drf_spectacular',
